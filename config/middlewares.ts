d69afe7eb28e6cd535fdfbe3e79ea57dce247e65
Pritesh Bavaskar <95741033+Pritesh-Bavaskar@users.noreply.github.com>
2025-06-24 12:34:43 +0530

    Configure CORS middleware with custom settings
    Updated the Strapi CORS middleware to explicitly allow requests from specified frontend URLs, set allowed methods, enable credentials, and allow all headers. This enhances security and ensures proper cross-origin communication between the backend and frontend applications.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,25 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: [
+        "http://localhost:3000",
+        "https://quambiant.com",
+        "https://staging.quambiant.com",
+      ], // 👈 your frontend URL
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      headers: "*",
+      credentials: true,
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
