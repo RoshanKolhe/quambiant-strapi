c13d29aa0fa9291cdbb586e5a4e2aeebce1c84dd
Pritesh Bavaskar <95741033+Pritesh-Bavaskar@users.noreply.github.com>
2025-07-22 12:22:02 +0530

    Add blog-subscribe content type and API

---
+++ src/api/blog-subscribe/controllers/blog-subscribe.ts
@@ -0,0 +1,7 @@
+/**
+ * blog-subscribe controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::blog-subscribe.blog-subscribe');
---
+++ src/api/blog-subscribe/routes/blog-subscribe.ts
@@ -0,0 +1,7 @@
+/**
+ * blog-subscribe router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::blog-subscribe.blog-subscribe');
